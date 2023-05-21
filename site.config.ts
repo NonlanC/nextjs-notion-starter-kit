cd29dc7552d87a5b141ac5d8c7d75cfd59059aac
NonlanC <78628338+NonlanC@users.noreply.github.com>
2023-05-21 22:26:19 +0800

    Update site.config.ts
    chgange

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '243431c6584649bc84e6af1e36a3d248',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Links',
+  domain: 'https://www.notion.so/nonlan/Nonlan-Links-243431c6584649bc84e6af1e36a3d248',
+  author: 'Nonlan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'owoNonlan',
+  github: 'NonlanC',
+  linkedin: 'nonlan-c-3a1754277',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
